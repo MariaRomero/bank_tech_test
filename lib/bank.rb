9a5f5c13436e03c04b041fa22d5ffd3732c7e864
MariaRomero <fantasysystems@gmail.com>
2016-06-28 11:37:15 +0100

    deposit method/test passing

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,4 +1,11 @@
 class Bank
 
+  def initialize
+    @balance = 0
+  end
+    
+  def deposit(amount)
+    return amount
+  end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,5 +1,12 @@
 require "bank"
 
 describe Bank do
-  
+bank = Bank.new
+  describe "#deposit" do
+    context "add amount" do
+      it "returns amount in hash" do
+        expect(bank.deposit(500)).to eq(500)
+      end
+    end
+  end
 end
